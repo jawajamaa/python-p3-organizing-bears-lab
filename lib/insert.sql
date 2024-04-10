49b9e8b295274db05782ff6f82f3956c13092330
Timothy Ryon <tim@timryon.com>
2024-04-10 13:22:36 -0400

    finish lab and pass all tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Mr.Chocolate',5,'M','brown','warm',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Rowdy',2,'M','brindle','rowdy',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Tabitha',25,'F','black','nurturing',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Sergeant Brown',44,'M','brown','authoritative',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Melissa',33,'F','brown','friendly',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Grinch',55,'M','brown and silver','grouchy',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Wendy',5,'F','brown','kind',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES(Null,1,'M','brown','cuddly',1);
